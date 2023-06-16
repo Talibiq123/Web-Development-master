e482f90355b6d264fc747d476aef3077f5581b9f
Talibiq123 <talibsaeed90@gmail.com>
2023-06-16 09:53:44 +0530

    commit

---
+++ JavaScript Basics/Basics/main.js
@@ -0,0 +1,21 @@
+//Declaration: allocation memory/space to a variable
+//Initialization: assigning value to a variable
+
+//Intractions in JS: alert, prompt, confirm
+//alert
+// alert("Something went wrong");
+
+//prompt
+// prompt(text, [default])
+// var value = parseInt(prompt("Type age"));
+// console.log(typeof value, value);
+
+// if(value > 18) {
+//     console.log("Eligible")
+// } else{
+//     console.log("not eligible")
+// }
+
+//confirm
+// var result = confirm("Is user Adult?");
+// console.log(result);
