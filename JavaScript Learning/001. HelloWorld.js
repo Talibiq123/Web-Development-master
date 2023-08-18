011725df7815573326b3b8233b0d1527bf1b7ab1
Talibiq123 <talibsaeed90@gmail.com>
2023-08-18 14:49:30 +0530

    Web devs

---
+++ JavaScript Learning/001. HelloWorld.js
@@ -0,0 +1,4 @@
+// console.log used to print anything on COnsole
+console.log("Hello, World!"); // double quotes
+console.log('My name is Mohd Talib.') //single quotes
+console.log(`I live in Bijnor`) // Backticks
---
+++ JavaScript Learning/002. Variables.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+// Variables
+var firstName = "Mohd"
+console.log(firstName)
+
+// firstName = "Talib"
+// console.log(firstName)
+
+// variable can be made without var, but not good.
+
+// Rules of declare variables
+// 1. cannot start withnumbers
+// 2. can use only underscore & Dollar signs
+// 3. cannot use spaces
+// 4. start with small letters and use Camelcase
+
+var middleName = "Talib";
+console.log(middleName)
+
+var $lastName = "Saeed"
+console.log($lastName)
+
+// first_name = "Talib" (snake case)
+// firstName = "Talib" (Camel case)
+
+
+// Let
+let myname = "Mohd Talib";
+console.log(myname)
+
+
+//const: values caan not be changed
+const pi = 3.14;
+console.log(pi);
+
