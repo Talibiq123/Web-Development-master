fcd1bd35c222cf3238c3a2c3cc31758c877e7d72
Talibiq123 <talibsaeed90@gmail.com>
2023-09-08 15:02:31 +0530

    JS End

--- JavaScript Learning/009. Asynchronous Programming/004. AJAX.js
+++ JavaScript Learning/009. Asynchronous Programming/004. AJAX.js
@@ -94,31 +94,95 @@
 
 
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
+// const url = 'https://jsonplaceholder.typicode.com/posts';
 
-function sendRequest(method, url) {
-    return new Promise( function(resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = function() {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.responseText);
-            } else {
-                reject(new Error("Something went wrong"));
-            }
-        }
-        xhr.onerror = function() {
-            reject(new Error("Something went wrong"));
-        }
-        xhr .send();
-    })
-}
+// function sendRequest(method, url) {
+//     return new Promise( function(resolve, reject) {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(method, url);
+//         xhr.onload = function() {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject(new Error("Something went wrong"));
+//             }
+//         }
+//         xhr.onerror = function() {
+//             reject(new Error("Something went wrong"));
+//         }
+//         xhr .send();
+//     })
+// }
 
 // const whatIsThis = sendRequest("GET", url);
 // console.log(whatIsThis);
 
-sendRequest("GET", url)
-.then(reponse => {
-    const data = JSON.parse(reponse);
-    console.log(data);
-})
+// sendRequest("GET", url)
+// .then(reponse => {
+//     const data = JSON.parse(reponse);
+//     console.log(data);
+// })
+
+
+
+
+
+// fetch
+// const url = 'https://jsonplaceholder.typicode.com/posts';
+// const whatIsThis = fetch(url);
+// console.log(whatIsThis);
+
+// fetch(url)
+// .then(response => {
+//     // console.log(Response);
+//     if (response.ok) {
+//         return response.json();
+//     } else {
+//         console.log("something went wrong");
+//     }
+// })
+// .then(data => {
+//     console.log(data);
+// })
+// .catch(err => {
+//     console.log("Inside catch...");
+//     console.log(err);
+// });
+
+
+// let arr = [1, 2, 3, 4];
+// function double(item) {
+//     return item * 2;
+// }
+
+// let val1 = arr.forEach(double);
+
+// let val2 = arr.map(double);
+
+// // questions
+// console.log(val1);
+// console.log(val2);
+
+// console.log(val2.join(""));
+// console.log(val1.join(""));
+
+
+
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+// fetch(url)
+// .then(response => response.json())
+// .then(data => console.log(data))
+
+
+// asynch await
+async function getPost() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+getPost().then(data => console.log(data)).catch(err => console.log(err));
+// const returned = getPost();
+// console.log(returned);
